861c91da2da585c9a2201d471ae9a5a52e7de34b
Igor M Farhan <igormfarhan2@gmail.com>
2020-08-08 05:52:35 +0700

    update tugas15

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import './App.css';
-import Tugas11 from './tugas11/tugas11.js'
-import Timer from './tugas12/timer.js'
-import CRUD from './tugas13/crud.js'
-import Hooks from './latihan14/hooks.js'
-import Lists from './latihan14/Lists';
-import HargaBuah from './tugas14/HargaBuah.js'
-
-let dataHargaBuah = [
-  {nama: "Semangka", harga: 10000, berat: 1000},
-  {nama: "Anggur", harga: 40000, berat: 500},
-  {nama: "Strawberry", harga: 30000, berat: 400},
-  {nama: "Jeruk", harga: 30000, berat: 1000},
-  {nama: "Mangga", harga: 30000, berat: 500}
-]
+import {BrowserRouter as Router} from 'react-router-dom'
+import Routes from './tugas15/Routes'
 
 function App() {
   return (
     <div>
-      {/* <Tugas11/>
-      <Timer countStart={100}/> */}
-      {/* <CRUD daftarBuah={dataHargaBuah}/> */}
-      <HargaBuah/>
+      <Router>
+        <Routes/>
+      </Router>
     </div>
   );
 }
-
 export default App;
